37fe87430d1b6f5cacaf66b14dcd15acfa69cd60
stanbogdan445 <stanbogdan445@yahoo.com>
2025-07-11 14:52:24 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
-import { useState } from "react";
 import Navbar from "./Components/Navbar";
 import NewBoard from "./Components/NewsBoard";
+import FavoritesPage from "./Components/FavoritesPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { FavoriteProvider } from "./FavoriteContext";
 
 export default function App() {
   const [category, setCategory] = useState("general");
+
   return (
-    <div>
-      <Navbar setCategory={setCategory} />
-      <NewBoard category={category} />
-    </div>
+    <FavoriteProvider>
+      <Router>
+        <Navbar setCategory={setCategory} />
+        <Routes>
+          <Route path="/" element={<NewBoard category={category} />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </Router>
+    </FavoriteProvider>
   );
 }
